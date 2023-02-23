78c97b4fd939ce3cb48c96f076dbad0ba3141b6e
WikiUhai <115320714+WikiUhai@users.noreply.github.com>
2023-02-23 10:24:03 -0600

    fix versioning

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-toolforge-jobs run uhaibot-2 --command "$HOME/pwbvenv/bin/python3 $HOME/sdlistupdate.py" --image python3.9 --schedule "22 * * * *" --retry 1
+toolforge-jobs run uhaibot-2 --command "$HOME/pwbvenv/bin/python3 $HOME/sdlistupdate.py" --image python3.7 --schedule "22 * * * *" --retry 1
