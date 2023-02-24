e1c57990aef78c8dc5b9a0468f0d38ae17bcd8d4
WikiUhai <115320714+WikiUhai@users.noreply.github.com>
2023-02-24 07:12:59 -0600

    correct edge case

--- sdlistupdate.py
+++ sdlistupdate.py
@@ -14,29 +14,30 @@ page = pwb.Page(site, 'User:Uhai/Pages_without_short_descriptions_by_view_count'
 table = re.findall(table_pattern, page.text)[0][0]
 entries = re.findall(item_pattern, table)
 
-in_subst = ', '.join(list(map(lambda x: '%s', entries)))
-query = "SELECT p.page_title FROM page p WHERE p.page_namespace = 0 AND (p.page_is_redirect = 1 OR p.page_id IN (SELECT cl.cl_from FROM categorylinks cl WHERE cl.cl_to = 'Articles_with_short_description')) AND p.page_title IN (%s);" % in_subst
+if len(entries) > 0:
+    in_subst = ', '.join(list(map(lambda x: '%s', entries)))
+    query = "SELECT p.page_title FROM page p WHERE p.page_namespace = 0 AND (p.page_is_redirect = 1 OR p.page_id IN (SELECT cl.cl_from FROM categorylinks cl WHERE cl.cl_to = 'Articles_with_short_description')) AND p.page_title IN (%s);" % in_subst
 
-conn = pymysql.connect(
-    host=host,
-    read_default_file=credentials,
-    database="enwiki_p"
-)
+    conn = pymysql.connect(
+        host=host,
+        read_default_file=credentials,
+        database="enwiki_p"
+    )
 
-with_sd = []
+    with_sd = []
 
-with conn.cursor() as cur:
-    cur.execute(query, args=entries)
-    data = cur.fetchall()
+    with conn.cursor() as cur:
+        cur.execute(query, args=entries)
+        data = cur.fetchall()
 
-    for row in data:
-        with_sd.append(str(row[0], encoding='utf-8'))
+        for row in data:
+            with_sd.append(str(row[0], encoding='utf-8'))
 
-conn.close()
+    conn.close()
 
-if len(with_sd) > 0:
-    for entry in with_sd:
-        delete_pattern = rf'\| \[\[{re.escape(entry)}\]\] \|\| [0-9]+\n\|-\n'
-        page.text = re.sub(delete_pattern, '', page.text)
+    if len(with_sd) > 0:
+        for entry in with_sd:
+            delete_pattern = rf'\| \[\[{re.escape(entry)}\]\] \|\| [0-9]+\n\|-\n'
+            page.text = re.sub(delete_pattern, '', page.text)
 
-    page.save('Removed ' + str(len(with_sd)) + ' article' + ('s' if len(with_sd) > 1 else '') + ' with short description added (bot)')
+        page.save('Removed ' + str(len(with_sd)) + ' article' + ('s' if len(with_sd) > 1 else '') + ' with short description added (bot)')
